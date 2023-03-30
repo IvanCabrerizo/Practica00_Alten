fae507332b23fdfec38122dff0c6d3540eb7166e
icabrerizo <ivan.cabrerizo@alten.es>
2023-03-30 12:45:01 +0200

    Terminado hasta la tarea 6 y comenzada la 7

--- app/src/main/java/com/example/practica0/Excercises.kt
+++ app/src/main/java/com/example/practica0/Excercises.kt
@@ -6,14 +6,17 @@ package com.example.practica0
  * @author Ivan Cabrerizo
  */
 
-fun main(){
+fun main() {
 
     tarea1(9229)
     tarea2Uno(10, 34, 5, 1, 20, 34)
     tarea2Dos(10, 34, 5, 1, 20, 34)
-    tarea2Tres(10, 34, 5, 1, 2 ,34)
+    tarea2Tres(10, 34, 5, 1, 2, 34)
     tarea3("Paco", "Juan", "Pedro", "Supercalifragilisticoespialidoso")
     tarea4(10)
+    tarea5("Paco23.")
+    tarea6("Ivan")
+    tarea7(p1 = listOf("Piedra", "Piedra", "Papel"), p2 = listOf("Papel", "Tijera", "Piedra"))
 }
 
 /**
@@ -21,20 +24,19 @@ fun main(){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea1(num: Int){
+fun tarea1(num: Int) {
     val numText = num.toString()
     val sizeText = numText.length
     var boolean = true
-    for (i in 0 until sizeText / 2){
-        if(numText[i] != numText[sizeText - i - 1]){
+    for (i in 0 until sizeText / 2) {
+        if (numText[i] != numText[sizeText - i - 1]) {
             boolean = false
         }
     }
 
-    if (boolean){
+    if (boolean) {
         println("$num es un numero capicua")
-    }
-    else{
+    } else {
         println("$num no es un numero capicua")
     }
 }
@@ -44,11 +46,11 @@ fun tarea1(num: Int){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea2Uno(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
+fun tarea2Uno(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int) {
     val listNum = listOf(num1, num2, num3, num4, num5, num6)
     var media = 0
 
-    for(i in listNum){
+    for (i in listNum) {
         media += i
     }
     media /= listNum.size
@@ -60,7 +62,7 @@ fun tarea2Uno(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea2Dos(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
+fun tarea2Dos(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int) {
     //Moda:
     val listNum = listOf(num1, num2, num3, num4, num5, num6)
     var actualNum = 0
@@ -68,16 +70,16 @@ fun tarea2Dos(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
     var saveCount = 0
     var moda = 0
 
-    for(i in listNum){
+    for (i in listNum) {
         actualNum = i
 
-        for (a in listNum){
-            if (actualNum == a){
+        for (a in listNum) {
+            if (actualNum == a) {
                 actualCount++
             }
         }
 
-        if(actualCount >= saveCount){
+        if (actualCount >= saveCount) {
             moda = actualNum
             saveCount = actualCount
         }
@@ -94,19 +96,18 @@ fun tarea2Dos(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea2Tres(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int){
+fun tarea2Tres(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int) {
     //La mediana:
     val listNum = listOf(num1, num2, num3, num4, num5, num6)
     var mediana = 0
     val orderList = listNum.sortedDescending()
 
-    if(orderList.size % 2 == 0){
+    if (orderList.size % 2 == 0) {
         val m1 = orderList.size / 2
         val m2 = (orderList.size / 2) + 1
         mediana = (orderList[m1] + orderList[m2]) / 2
         println("La mediana es : $mediana")
-    }
-    else{
+    } else {
         val medio = (orderList.size - 1) / 2
         mediana = orderList[medio + 1]
         println("La mediana es : $mediana")
@@ -119,13 +120,13 @@ fun tarea2Tres(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int)
  *
  * @author Ivan Cabrerizo
  */
-fun tarea3(s1: String, s2: String, s3: String, s4: String){
+fun tarea3(s1: String, s2: String, s3: String, s4: String) {
     val listString = listOf<String>(s1, s2, s3, s4)
     var mayor = 0
     var posicion = 0
 
-    for (i in listString.indices){
-        if(listString[i].length > mayor ){
+    for (i in listString.indices) {
+        if (listString[i].length > mayor) {
             mayor = listString[i].length
             posicion = i
         }
@@ -139,10 +140,10 @@ fun tarea3(s1: String, s2: String, s3: String, s4: String){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea4(num: Int){
+fun tarea4(num: Int) {
     var secuencia = mutableListOf<Int>(0, 1)
 
-    for (i in 0 until num){
+    for (i in 0 until num) {
         val fibonacci = secuencia[i] + secuencia[i + 1]
         secuencia.add(fibonacci)
     }
@@ -155,6 +156,74 @@ fun tarea4(num: Int){
  *
  * @author Ivan Cabrerizo
  */
-fun tarea5(encrypt: String){
-    val hackerKey = mapOf('a' to 'a')
+fun tarea5(encrypt: String) {
+    val hackerKey = mapOf(
+        "a" to "4",
+        "b" to "I3",
+        "c" to "[",
+        "d" to ")",
+        "e" to "3",
+        "f" to "|=",
+        "g" to "&",
+        "h" to "#",
+        "i" to "1",
+        "j" to ",_|",
+        "k" to ">|",
+        "l" to "1",
+        "k" to ">|",
+        "m" to "/\\/\\",
+        "n" to "^/",
+        "o" to "0",
+        "p" to "|*",
+        "q" to "(_,)",
+        "r" to "I2",
+        "s" to "5",
+        "t" to "7",
+        "u" to "(_)",
+        "v" to "\\/",
+        "w" to "\\/\\/",
+        "x" to "><",
+        "x" to "j",
+        "y" to "2",
+        "1" to "L",
+        "2" to "R",
+        "3" to "E",
+        "4" to "A",
+        "5" to "S",
+        "6" to "b",
+        "7" to "T",
+        "8" to "B",
+        "9" to "g",
+        "0" to "o")
+
+    var wordEncrypted = ""
+
+    for(i in encrypt){
+        val translate  = hackerKey[i.lowercase()]
+        wordEncrypted += translate ?: ""
+    }
+
+    println("La palabra secreta es: $wordEncrypted")
+}
+
+/**
+ * Funcion que transforma una cadena en su version espejo
+ *
+ * @author Ivan Cabrerizo
+ */
+fun tarea6(word: String){
+    println("La palabra modificada seria: $word${word.dropLast(1).reversed()}")
+}
+
+/**
+ * Funcion que transforma una cadena en su version espejo
+ *
+ * @author Ivan Cabrerizo
+ */
+fun tarea7(p1: List<String>, p2: List<String>){
+    for(i in p1.indices){
+        when {
+            p1[i] == "Piedra" && p2[i] == "Papel" -> println("Hola")
+        }
+    }
 }
